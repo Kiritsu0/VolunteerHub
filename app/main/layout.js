bd277084e45fd1893860ac13a7a602bc2d26904c
Kiritsu <deebh776@gmail.com>
2024-02-03 21:52:07 +0200

    modify header

--- app/main/layout.js
+++ app/main/layout.js
@@ -1,26 +1,23 @@
-import { IoIosSearch } from "react-icons/io";
 import { IoMdSettings } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
+import { IoListOutline } from "react-icons/io5";
 
 const Layout = ({children}) => {
   return (
     <div>
-      <header className="flex justify-around items-center w-screen bg-white min-h-14">
-        <h1 className="text-4xl font-medium">Volunteer<span className="font-bold text-green-600">Hub</span></h1>
-        <div className="flex items-center bg-gray-200 w-1/4 p-1 gap-3 rounded-3xl">
-          <IoIosSearch className="text-3xl ml-3"/>
-          <input placeholder="Search VolunteerHub" className="outline-none bg-gray-200 w-full"/>
-        </div>
-        <div className="flex items-center gap-3">
+      <header className="flex justify-between items-center w-screen min-h-14">
+        <h1 className="font-medium ml-8 text-2xl sm:text-2xl md:text-3xl lg:text-4xl">Volunteer<span className="font-bold text-green-600">Hub</span></h1>
+        <div className="flex items-center gap-5 mr-8">
           <div className="group relative">
-            <IoMdSettings className="text-3xl bg-gray-200 rounded-full p-1 cursor-pointer"/>
+            <IoMdSettings className=" bg-gray-200 rounded-full p-1 cursor-pointer text-2xl sm:text-2xl lg:text-3xl"/>
             <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
               Settings
             </span>
           </div>
-          <div className="group relative flex items-center gap-1">
-            <FaUser className="text-3xl bg-gray-200 rounded-full p-1 cursor-pointer"/>
-            <span className="font-medium">User</span>
+
+          <div className="group relative flex items-center gap-3 border-slate-400 border-2 shadow-md shadow-slate-500 cursor-pointer py-1 px-2 rounded-3xl hover:shadow-xl">
+            <IoListOutline className="text-2xl"/>
+            <FaUser className="bg-gray-200 rounded-full p-1 text-2xl sm:text-2xl md:text-3xl lg:text-4xl"/>
             <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
               Account
             </span>
--- app/main/volunteer/page.js
+++ app/main/volunteer/page.js
@@ -1,20 +1,29 @@
 import { FaUser } from "react-icons/fa";
 import { FaCirclePlus } from "react-icons/fa6";
+import { IoIosSearch } from "react-icons/io";
+
 
 const Volunteer = () => {
   return (
     <div>
-      <div className="bg-white w-1/4 h-96 p-5">
-        <div className="flex items-center gap-1">
-          <FaUser className="text-4xl bg-gray-200 rounded-full p-1"/>
-          <span>User</span>
-        </div>
-
-        <hr className="my-3"/>
-
-        <div className="flex items-center gap-2">
-          <h1 className="text-2xl font-semibold">Skills</h1>
-          <button><FaCirclePlus className="text-green-600 text-3xl"/></button>
+      <div className="flex justify-center mt-5">
+        <div className="bg-white flex w-1/2 rounded-full cursor-pointer">
+          <div className="rounded-full hover:bg-slate-300 h-full py-3 pl-5">
+            <h2>Region</h2>
+            <input placeholder="Enter Location" className="bg-transparent outline-none"/>
+          </div>
+          <div className="rounded-full hover:bg-slate-300 h-full py-3 pl-5">
+            <h2>Skills</h2>
+            <input placeholder="Add Skills" className="bg-transparent outline-none"/>
+          </div>
+          <div className="rounded-full hover:bg-slate-300 h-full py-3 pl-5 ">
+            <h2>Duration</h2>
+            <input type="date" className="bg-transparent outline-none"/>
+          </div>
+          <button className="rounded-full hover:bg-slate-300 h-full py-3 pl-5 flex-1 flex items-center gap-2">
+            <IoIosSearch className="text-2xl"/>
+            Search
+          </button>
         </div>
       </div>
     </div>
