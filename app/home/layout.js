a42aabc88aa5553a442d6c041a69e3344fa27cb6
Kiritsu <deebh776@gmail.com>
2024-02-06 21:36:42 +0200

    modify code

--- app/home/layout.js
+++ app/home/layout.js
@@ -3,7 +3,15 @@ import { FaUser } from "react-icons/fa";
 import { CiLogout } from "react-icons/ci";
 import { signOut, useSession } from "next-auth/react";
 
+import {useState} from "react"
+import Link from "next/link"
+
 const Layout = ({ children }) => {
+  const [showDropdown, setDropdown] = useState(false)
+
+  const dropdown = () => {
+    setDropdown(!showDropdown)
+  }
   const signout = () => {
     console.log("signout function called");
     signOut();
@@ -19,19 +27,22 @@ const Layout = ({ children }) => {
           Volunteer<span className="font-bold text-green-600">Hub</span>
         </h1>
         <div className="flex items-center gap-5 mr-8">
-          <div onClick={signout} className="group relative">
-            <CiLogout className=" bg-gray-200 rounded-full p-1 cursor-pointer text-2xl sm:text-2xl lg:text-3xl" />
-            <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
-              LogOut
-            </span>
-          </div>
-
-          <div className="group relative flex items-center gap-3 border-slate-400 border-2 shadow-md shadow-slate-500 cursor-pointer py-1 px-2 rounded-3xl hover:shadow-xl">
+          <div onClick={dropdown} className="group relative flex items-center gap-3 border-slate-400 border-2 shadow-md shadow-slate-500 cursor-pointer py-1 px-2 rounded-3xl hover:shadow-xl">
             <FaUser className="bg-gray-200 rounded-full p-1 text-2xl sm:text-2xl md:text-3xl lg:text-4xl" />
             <h4>{userName}</h4>
             <span className="hidden group-hover:block absolute top-14 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-md text-sm">
               Account
             </span>
+            { showDropdown && ( 
+              <div className="absolute top-12 left-1/2 transform -translate-x-1/2 bg-white text-sm h-40">
+                <Link onClick={signout} href="" className="flex gap-2 items-center p-2 hover:bg-slate-200">
+                  <CiLogout className=" bg-gray-200 rounded-full p-1 cursor-pointer text-2xl sm:text-2xl lg:text-3xl" />
+                  Logout
+                </Link>
+                <hr />
+              </div>
+              )
+            }
           </div>
         </div>
       </header>
