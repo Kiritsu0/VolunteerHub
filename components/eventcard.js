f71f73a6848395c3276fc8f4215e79006bf35dd7
CYCNO <daniyalchunna2@gmail.com>
2024-02-13 19:57:45 +0530

    added back the cards

--- components/eventcard.js
+++ components/eventcard.js
@@ -1,20 +1,25 @@
+// eventcard.js
 "use client";
 import React from "react";
 
 const Eventcard = ({ image, title, desc }) => {
   return (
-    <div className="flex h-full w-full justify-center  pt-4 px-2">
-      <div className="flex h-68 w-[40rem] items-center rounded-md border-2 bg-gray-200 pr-2 shadow-xl">
-        <div className="h-full w-2/5">
+    <div className="flex h-full w-full justify-center pt-10">
+      <div className="flex h-[250px] w-[650px] items-center rounded-md border-2 bg-gray-200 pr-2 shadow-xl">
+        <div className="h-full w-2/5 rounded-md">
           <img
             src={image}
             alt="Description of the image"
-            className="object-cover w-full h-full rounded-l-md"
+            className="object-cover w-full h-full overflow-hidden rounded-sm"
+            style={{
+              borderTopLeftRadius: "10px",
+              borderBottomLeftRadius: "10px",
+            }}
           />
         </div>
-        <div className="mt-5 flex flex flex-col px-6 pb-5">
+        <div className="mt-5 flex h-[230px] w-[400px] flex-col px-6">
           <h1 className="line-clamp-2 text-xl font-semibold">{title}</h1>
-          <article className="line-clamp-3 mb-3">{desc}</article>
+          <h2 className="line-clamp-3 overflow-hidden pb-[3px]">{desc}</h2>
           <div className="mt-2 flex justify-between">
             <h3 className="font-semibold">Place: New York</h3>
             <h3 className="font-semibold">Date: 1/1/2024</h3>
